4f93fedca182c058e74bc44dcd799d8176519fe2
Sumaaaaaaaa <a1105504813@gmail.com>
2024-11-01 02:20:07 +0900

    [小推进]各种类的创建

---
+++ Assets/Model/Branch.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Model
+{
+    public class Branch
+    {
+        private List<BuiltinPhytomer> _builtinPhytomers;
+    }
+}
--- Assets/Model/Phytomer.cs
+++ Assets/Model/Phytomer.cs
@@ -1,8 +1,14 @@
-﻿
+﻿using UnityEngine;
 namespace Model
 {
     public class Phytomer
     {
         public string Name;
+    }
+
+    internal class BuiltinPhytomer
+    {
+        private Vector3 _position;
+        private Vector3 _direction;
     }
 }
